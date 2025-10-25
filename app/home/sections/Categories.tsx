3c79fa0e362f6ae9a8ce0a076c4ee8cb8f59e88b
mayklweb <muhammadevvv@gmail.com>
2025-10-25 13:57:57 +0500

    catrgories fix

--- app/home/sections/Categories.tsx
+++ app/home/sections/Categories.tsx
@@ -20,8 +20,11 @@ function Categories() {
     <section>
       <div className="container">
         <div className="mt-6 mb-10 grid grid-cols-4 gap-2 lg:gap-4">
-          {categories.map((category) => (
-            <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
+          {categories?.map(({ id, name }) => (
+            <div
+              key={id}
+              className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out"
+            >
               <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
                 <Image
                   src="/logo1.svg"
@@ -32,123 +35,10 @@ function Categories() {
                 />
               </div>
               <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-                Pechenie
+                {name}
               </p>
             </div>
           ))}
-
-          {/* <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div>
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
-            <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
-              <Image
-                src="/logo1.svg"
-                className="lg:p-1"
-                alt="categories"
-                width={50}
-                height={50}
-              />
-            </div>
-            <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
-              Pechenie
-            </p>
-          </div> */}
         </div>
       </div>
     </section>
