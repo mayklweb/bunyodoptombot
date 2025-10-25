ebf4b24ae6a18d129c6987b5ceb6536fac479005
mayklweb <muhammadevvv@gmail.com>
2025-10-25 12:54:52 +0500

    catrgories fix

--- app/home/sections/Categories.tsx
+++ app/home/sections/Categories.tsx
@@ -1,12 +1,43 @@
 import Image from "next/image";
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { CategoriesType } from "@/app/types";
+import { getCategories } from "@/app/api/apiServices"; // Ensure this path is correct
 
 function Categories() {
+  const {
+    data: categories,
+    isError,
+    error,
+    isLoading,
+  } = useQuery<CategoriesType>({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
+
+  console.log(categories);
+
   return (
     <section>
       <div className="container">
         <div className="mt-6 mb-10 grid grid-cols-4 gap-2 lg:gap-4">
-          <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
+          {categories.map((category) => (
+            <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
+              <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
+                <Image
+                  src="/logo1.svg"
+                  className="lg:p-1"
+                  alt="categories"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
+                Pechenie
+              </p>
+            </div>
+          ))}
+
+          {/* <div className="lg:bg-[#CECFFF] bg-transparent rounded-xl flex items-center justify-center flex-col lg:flex-row gap-1 lg:p-2 cursor-pointer hover:shadow-md transition-all duration-200 ease-in-out">
             <div className="p-2 bg-[#CECFFF] rounded-md flex items-center justify-center">
               <Image
                 src="/logo1.svg"
@@ -117,7 +148,7 @@ function Categories() {
             <p className="text-sm lg:text-lg font-medium text-[#2e3192]">
               Pechenie
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,11 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col">
-          <header></header>
-          <main className="container flex-auto">{children}</main>
-          <footer></footer>
-        </div>
+        <Providers>
+          <div className="flex flex-col">
+            <header></header>
+            <main className="container flex-auto">{children}</main>
+            <footer></footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
