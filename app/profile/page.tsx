08c397bbc2999c02c8bf6fd393449a194d946455
mayklweb <muhammadevvv@gmail.com>
2025-10-27 13:08:42 +0500

    profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { UserCircle } from "lucide-react";
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <section>
+      <div className="mt-[100px]">
+        <div className="flex items-center gap-2">
+          <div className="bg-[#F1F1F1] rounded-full">
+            <UserCircle size={32} strokeWidth={1.5} />
+          </div>
+          <h3 className="text-xl font-medium">Muhammad</h3>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Profile
+export default Profile;
