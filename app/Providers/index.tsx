7a8ca99bdb58a0e1293e7c15b8c0e6c4d40b1200
mayklweb <muhammadevvv@gmail.com>
2025-11-03 22:48:05 +0500

    update

--- app/Providers/index.tsx
+++ app/Providers/index.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useState } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.Telegram?.WebApp) {
-      const tg = window.Telegram.WebApp;
-      tg.ready();
-      tg.disableVerticalSwipes();
-      tg.expand();
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined" && window.Telegram?.WebApp) {
+  //     const tg = window.Telegram.WebApp;
+  //     tg.ready();
+  //     tg.disableVerticalSwipes();
+  //     tg.expand();
+  //   }
+  // }, []);
   
 
   return (
