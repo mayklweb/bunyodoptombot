f039c7e3095d9041a59f474af650e6ca83a9fb23
mayklweb <muhammadevvv@gmail.com>
2025-11-04 12:59:46 +0500

    m

--- app/products/[id]/ProductsClient.tsx
+++ app/products/[id]/ProductsClient.tsx
@@ -2,10 +2,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "@/app/shared/api/apiServices";
 import { ProductsType } from "@/app/types";
+import Image from "next/image";
 
 export default function ProductsClient({ id }: { id: number }) {
   const {
-    data: products, isError, error, isLoading,} = useQuery<ProductsType[], Error>({
+    data: products,
+    isError,
+    error,
+    isLoading,
+  } = useQuery<ProductsType[], Error>({
     queryKey: ["products"],
     queryFn: getProducts,
   });
@@ -17,7 +22,30 @@ export default function ProductsClient({ id }: { id: number }) {
 
   return (
     <section>
-      {product ? (
+      <div className="container">
+        <div className="mt-5">
+          <div>
+            <div className="rounded-2xl overflow-hidden w-full h-60">
+              <Image
+                className="w-full h-full object-cover"
+                src="/cookie.webp"
+                alt="product"
+                width={300}
+                height={200}
+                priority
+              />
+            </div>
+            <div>
+              <h1 className="text-2xl font-medium mt-4">Product Name</h1>
+              <div className="flex items-center justify-between mt-2">
+                <p className="text-lg font-semibold">25 000 USZ</p>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      {/* {product ? (
         <div>
           <h1>{product.name}</h1>
           <p>{product.price}</p>
@@ -25,7 +53,7 @@ export default function ProductsClient({ id }: { id: number }) {
         </div>
       ) : (
         <p>Product not found.</p>
-      )}
+      )} */}
     </section>
   );
 }
